9da52a7c639933b9e7b8bb4f3f980ac04b50d9fe
vasenkom <m.vasenko@yandex.ru>
2024-07-30 10:19:44 +0200

    build a test to check if duplicates are removed from the array and if the array is being return sorted

---
+++ binarySearchTree.test.js
@@ -0,0 +1,11 @@
+const { Node, Tree, buildTree, removeDuplicates } = require("./index");
+
+test("test the removeDuplicates()", () => {
+  expect(removeDuplicates([1, 2, 2, 3, 4, 5, 5, 7])).toEqual([
+    1, 2, 3, 4, 5, 7,
+  ]);
+});
+
+test("return sorted no duplicat function", () => {
+  expect(buildTree([1, 9, 2, 2, 3, 4, 5, 5, 7])).toEqual([1, 2, 3, 4, 5, 7, 9]);
+});
